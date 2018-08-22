ba4ef15660dc0cbdf1781717b08634b94caedd47
MirekTuruk <miroslaw.turuk@gmail.com>
2018-08-22 07:40:31 +0200

    event

---
+++ src/main/java/com/example/eventOrganizer/EventOrganizerApplication.java
@@ -0,0 +1,12 @@
+package com.example.eventOrganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventOrganizerApplication.class, args);
+	}
+}
